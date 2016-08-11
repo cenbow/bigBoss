a6ff4070f2826e84b9a11236b6344f10921d25e9
aiuluna <604401072@qq.com>
2016-08-11 23:29:47 +0800

    2016-08-11 commit tree name

--- server/ext_web/web/app/index/store/WestTreePanelStore.js
+++ server/ext_web/web/app/index/store/WestTreePanelStore.js
@@ -15,8 +15,8 @@ Ext.define('Index.store.WestTreePanelStore', {
         expanded: true,
         children: [
           {
-            text: '基础信息',
-            qtip: '基础信息',
+            text: '账号管理',
+            qtip: '账号管理',
             leaf: false,
             expanded: true,
             children: [
@@ -27,7 +27,7 @@ Ext.define('Index.store.WestTreePanelStore', {
                 id: 'accountMgmt',
                 leaf: true
               },
-              {
+              /*{
                 text: '店铺授权',
                 qtip: '店铺授权',
                 url: 'app-shopMgmt.html',
@@ -67,219 +67,76 @@ Ext.define('Index.store.WestTreePanelStore', {
                 url: 'app-companySetting.html',
                 id: 'companySetting',
                 leaf: true
-              }
+              }*/
             ]
           },
           {
-            text: '商品管理',
-            qtip: '商品管理',
+            text: '公司管理',
+            qtip: '公司管理',
             leaf: false,
             expanded: true,
             children: [
               {
-                text: '商品资料',
-                qtip: '商品资料',
+                text: '公司管理',
+                qtip: '公司管理',
                 url: 'app-productMgmt.html',
-                id: 'productMgmt',
+                id: 'companyMgmt',
+                leaf: true
+              }
+            ]
+          },
+          {
+            text: '分类管理',
+            qtip: '分类管理',
+            leaf: false,
+            expanded: true,
+            children: [
+              {
+                text: '信息披露',
+                qtip: '信息披露',
+                url: 'app-productMgmt.html',
+                id: 'infoDisclosure',
                 leaf: true
               },
               {
-                text: '回收站',
-                qtip: '回收站',
+                text: '学习园地',
+                qtip: '学习园地',
                 url: 'app-productRecycled.html',
-                id: 'productRecycled',
+                id: 'studyGarden',
+                leaf: true
+              },
+              {
+                text: '市场资讯',
+                qtip: '市场资讯',
+                url: 'app-productRecycled.html',
+                id: 'marketInfo',
+                leaf: true
+              },
+              {
+                text: '通知公告',
+                qtip: '通知公告',
+                url: 'app-productRecycled.html',
+                id: 'noticeShow',
                 leaf: true
               }
             ]
           },
           {
-            text: '销售管理',
-            qtip: '销售管理',
+            text: '信息管理',
+            qtip: '信息管理',
             leaf: false,
             expanded: true,
             children: [
               {
-                text: '手工开单',
-                qtip: '手工开单',
-                url: 'app-orderManually.html',
-                id: 'orderManually',
-                leaf: true
-              },
-              {
-                text: '平台订单',
-                qtip: '平台订单',
-                url: 'app-shopOrderList.html',
-                id: 'shopOrderList',
-                leaf: true
-              },
-              {
-            	  text: '订单客审',
-                qtip: '订单客审',
-                url: 'app-orderReview.html',
-                id: 'orderReview',
-                leaf: true
-              },
-              {
-            	text: '订单财审',
-                qtip: '订单财审',
-                url: 'app-orderFinancialReview.html',
-                id: 'orderFinancialReview',
-                leaf: true
-              },
-              {
-                text: '订单查询',
-                qtip: '订单查询',
-                url: 'app-orderQuery.html',
-                id: 'orderQuery',
-                leaf: true
-              },
-              {
-                text:'异常订单',
-                qtip: '订单查询',
-                url: 'app-orderHold.html',
-                id: 'orderHold',
-                leaf: true
-              },
-              {
-                text: '售后单查询',
-                qtip: '售后单查询',
-                leaf: true
-              }
-            ]
-          },
-          {
-            text: '库存管理',
-            qtip: '库存管理',
-            leaf: false,
-            expanded: true,
-            children: [
-              {
-                text: '仓库管理',
-                qtip: '仓库管理',
-                url: 'app-whsMgmt.html',
-                id: 'whsMgmt',
-                leaf: true
-
-              },
-              {
-                text: '库位管理',
-                qtip: '库位管理',
-                url: 'app-pickLocation.html',
-                id: 'pickLocation',
-                leaf: true
-
-              },
-              {
-                text: '库存调拨',
-                qtip: '库存调拨',
-                url: 'app-stockTransfer.html',
-                id: 'stockTransfer',
-                leaf: true
-              },
-              {
-                text: '杂项入库',
-                qtip: '杂项入库',
-                url: 'app-goodsReceipt.html',
-                id: 'goodsReceipt',
-                leaf: true
-              },
-              {
-                text: '杂项出库',
-                qtip: '杂项出库',
-                url: 'app-goodsIssue.html',
-                id: 'goodsIssue',
-                leaf: true
-              },
-              {
-                text: '采购收货',
-                qtip: '采购收货',
-                url: 'app-purchaseReceipt.html',
-                id: 'purchaseReceipt',
-                leaf: true
-              },
-              {
-                text: '采购退货',
-                qtip: '采购退货',
-                url: 'app-purchaseReturn.html',
-                id: 'purchaseReturn',
-                leaf: true
-              },
-              {
-                text: '库存盘点',
-                qtip: '库存盘点',
-                leaf: true,
-                url: 'app-stockTaking.html',
-                id: 'stockTaking'
-              }
-            ]
-          },
-          {
-            text: '采购管理',
-            qtip: '采购管理',
-            leaf: false,
-            expanded: true,
-            children: [
-              {
-                text: '采购订单',
-                qtip: '采购订单',
-                leaf: true,
-                url: 'app-purchaseOrder.html',
-                id: 'purchaseOrder'
-              },
-              {
-                text: '采购结款',
-                qtip: '采购结款',
-                leaf: true,
-                url: 'app-purchasePayment.html',
-                id: 'purchasePayment'
-              }
-            ]
-          },
-          {
-            text: '报表分析',
-            qtip: '报表分析',
-            leaf: false,
-            expanded: true,
-            children: [
-              {
-                text: '销售订单统计报表',
-                qtip: '销售订单统计报表',
-                leaf: true
-              },
-              {
-                text: '商品资料统计表表',
-                qtip: '商品资料统计表表',
-                leaf: true
-              },
-              {
-                text: '出入库明细报表',
-                qtip: '出入库明细报表',
-                leaf: true,
-                url: 'app-stockJournalSummary.html',
-                id: 'stockJournalSummary'
-              },
-              {
-                text: '进销存统计报表',
-                qtip: '进销存统计报表',
-                leaf: true,
-                url: 'app-stockInvoicingSummary.html',
-                id: 'stockInvoicingSummary'
-              }
-            ]
-          },
-          {
-            text: '海关设置',
-            qtip: '库存管理',
-            leaf: false,
-            expanded: false,
-            children: [
-              {
-                text: '海关备案设置',
-                qtip: '海关备案设置',
+                text: '信息管理',
+                qtip: '信息管理',
+                url: 'app-productMgmt.html',
+                id: 'infoMgmt',
                 leaf: true
               }
             ]
           }
+
         ]
       },
       fields: [
